9a8385d53ee2efbd000a412b122cda188c80a7cf
Joe - RLO | ShardDog <103778941+joe-rlo@users.noreply.github.com>
2023-08-04 18:14:51 -0400

    Added headers for CORS

--- api/proof.py
+++ api/proof.py
@@ -79,6 +79,10 @@ class handler(BaseHTTPRequestHandler):
 
 		self.send_response(200)
 		self.send_header('Content-type', 'application/json')
+		self.send_header('Access-Control-Allow-Origin', '*')
+		self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+		self.send_header('Access-Control-Allow-Headers', 'X-Requested-With')
+		self.send_header('Access-Control-Allow-Headers', 'Content-Type')
 		self.end_headers()
 		self.wfile.write(json.dumps(metadata).encode())
 		return
